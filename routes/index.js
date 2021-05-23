18adaf67b8d13ea4cb8296746a01aabc2c7d05c8
rafaelabonacim <rafaelabonacim@gmail.com>
2021-05-23 18:47:30 -0300

    front do cadastro

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 // Importação das Controllers
 const institutionalController = require('../controllers/institutionalController');
 const loginController = require('../controllers/loginController');
+const cadastroController = require('../controllers/cadastroController');
 
 // Rotas para páginas institucionais
 router.get('/', institutionalController.index);
@@ -12,6 +13,9 @@ router.get('/anuncie', institutionalController.anuncie);
 
 // Rotas para páginas de login
 router.get('/login',loginController.login);
+
+// Rotas para páginas de cadastro
+router.get('/register-type',cadastroController.cadastro);
 
 
 
